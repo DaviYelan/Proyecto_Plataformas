a1af7a57ed3e12f3b551f4fe46be36e46ad28de0
Miguelin04 <miguel.a.luna@unl.edu.ec>
2025-10-28 07:44:36 -0500

    cambios en el nombre de algunas importaciones

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from .my_routes.route import router
-from .my_routes.router_usuario import router_usuario
-from .my_routes.route_bus import router_bus
-from .my_routes.route_admin import router_admin
+from .router.route import router
+from .router.router_usuario import router_usuario
+from .router.route_bus import router_bus
+from .router.route_admin import router_admin
 from .config import API_URL
 
 
